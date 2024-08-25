f6e4f19e349455190e0f189161c9510cbe93376a
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2024-08-25 14:21:50 +0300

    popup

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@
 import * as header from './modules/header.js';
 import * as droplist from './modules/droplist.js';
 import * as clients from './modules/clients.js';
+import * as popup from './modules/popup.js';
 
 
 // flsFunctions.isWebp()
